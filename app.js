ac1e01ec4fe0994ce350ac68230537519a504113
Sierra Scott <sierrascott@Sierras-MacBook-Pro.local>
2021-01-19 06:44:53 -0800

    Add git ignore, Procfile, and update ports

--- app.js
+++ app.js
@@ -162,6 +162,11 @@ app.post("/delete", function(req, res) {
 
 });
 
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started");
 });
